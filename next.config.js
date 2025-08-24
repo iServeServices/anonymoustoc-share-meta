fc8338282ec8bc559b7a038f1a1da9448e2d8bfa
iServeServices <iserve.serviceproviders@gmail.com>
2025-08-24 16:56:53 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // no deprecated experimental.appDir here
-  images: { unoptimized: true }, // safe default; remove if you don't need
-};
-
-module.exports = nextConfig;
